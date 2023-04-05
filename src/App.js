9fd8a7f159b73b924ca14b36a53c49974929e0a8
abrarasif11 <faasif929@gmail.com>
2023-04-05 21:47:51 +0600

    router-setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Main from './component/Layout/Main';
+import Home from './component/Home/Home';
+import Contact from './component/Contact/Contact';
+import Items from './component/Items/Items';
+import AllItems from './component/Items/AllItems';
 
 function App() {
+  const router = createBrowserRouter([
+    {
+     path : '/',
+     element : <Main></Main>,
+     children : [
+      {
+        path : '/',
+        element : <Home></Home>
+      },
+      {
+        path : '/',
+        element : <Home></Home>
+      },
+      {
+        path : '/contact',
+        element : <Contact></Contact>
+      },
+      {
+        path : '/items',
+        element : <Items></Items>
+      },
+      {
+        path : '/allItems',
+        element : <AllItems></AllItems>
+      },
+      {
+        path : '/about',
+        element : <Home></Home>
+      },
+     ]
+    }
+  ])
   return (
     <div className="App">
-     
+     <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
