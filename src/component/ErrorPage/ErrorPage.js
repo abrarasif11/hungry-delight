dbd6cae58a1a198d76e0a02f2dffe137e83b0678
MD Fahim Abrar Asif <faasif929@gmail.com>
2025-10-23 20:41:23 +0600

    Update ErrorPage.js

--- src/component/ErrorPage/ErrorPage.js
+++ src/component/ErrorPage/ErrorPage.js
@@ -7,7 +7,7 @@ const ErrorPage = () => {
             <div className="container flex flex-col items-center justify-center px-5 mx-auto my-8">
                 <div className="max-w-md text-center">
                     <h2 className="mb-8 font-extrabold text-9xl text-gray-600">
-                        <span className="sr-only">Error</span>404
+                        <span className="sr-only">Error Page</span>404
                     </h2>
                     <p className="text-2xl font-semibold md:text-3xl">Sorry, we couldn't find this page.</p>
                     <p className="mt-4 mb-8 ">But don't worry, you can find plenty of other things on our homepage.</p>
